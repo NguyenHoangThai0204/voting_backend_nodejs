ee7e9bf6d6018a84f16b4ea1bd5c4bd61cfddf80
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-09-13 05:12:53 +0700

    done

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const { default: mongoose } = require('mongoose')
+const express = require('express');
+const app = express();
+const port = 3000;
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const routerUser = require("./src/routes/UserRoute")
-const routerPoll = require("./src/routes/PollRoute")
-const routerVote = require("./src/routes/VoteRoute")
+const routerUser = require("./src/routes/UserRoute");
+const routerPoll = require("./src/routes/PollRoute");
+const routerVote = require("./src/routes/VoteRoute");
 
-app.use(bodyParser.json({limit: '50mb'}))
+dotenv.config(); // Đọc file .env
 
-app.use(cors())
-app.use(morgan('common'))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
+app.use(morgan('common'));
 
-// 
+// Định nghĩa các route
+app.use('/api/user', routerUser);
+app.use('/api/poll', routerPoll);
+app.use('/api/vote', routerVote);
+
+// Route mặc định
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.use('/api/user',routerUser)
-
-app.use('/api/poll',routerPoll)
-
-app.use('/api/vote',routerVote)
-// Kết nối mongodb
+// Route không tìm thấy
 app.use((req, res) => {
   res.status(404).send("Route not found");
 });
 
-dotenv.config(); // dùng để đọc file .env
-
-const connectToMongoDB = async ()=>{
-  try{
-    await mongoose.connect(`${process.env.MONGO_DB}`);
+// Kết nối MongoDB
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
     console.log('Connected to MongoDB successfully!');
-  }catch (error ){
-    console.log('Error connecting to mongodb' , error.message);
+  } catch (error) {
+    console.log('Error connecting to MongoDB', error.message);
   }
-
-}
-
+};
 
 app.listen(port, () => {
   connectToMongoDB();
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-// app.get('/api', (req, res) => {
-//   res.send('Hello World!')
-// })
+  console.log(`Example app listening on port ${port}`);
+});
